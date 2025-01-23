8689fcbad820e1840a1ce46a24686d1a84090a61
grisoustyle <nicolaspfeifer293@gmail.com>
2025-01-23 17:56:23 +0100

    k1a4

---
+++ Altklausuren/1-WS2223/K1-Aufgabe4/K1-Aufgabe4.py
@@ -0,0 +1,13 @@
+def raute(breite):
+    spacer = breite//2
+    #print (f"spacer{spacer}")
+    for i in range (1,breite+1,2):
+        print((spacer*" ") + i*"X")
+        spacer -=1
+    spacer = 1
+    for i in range(breite-2,0,-2):
+        #print(i)
+        print((spacer*" ") + i*"X")
+        spacer +=1
+        
+raute(9)
