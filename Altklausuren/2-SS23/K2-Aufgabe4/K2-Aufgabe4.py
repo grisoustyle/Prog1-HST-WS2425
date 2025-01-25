2f60a9056f79aa84b32bae446e00272552cac7cc
grisoustyle <nicolaspfeifer293@gmail.com>
2025-01-25 21:04:15 +0100

    k2a4oben

---
+++ Altklausuren/2-SS23/K2-Aufgabe4/K2-Aufgabe4.py
@@ -0,0 +1,32 @@
+def stern(breite):
+    try:
+        if breite < 5:
+            raise Exception ("Mindestbreite unterschritten")
+        if breite%2 == 0:
+            raise Exception ("Breite muss ungerade Zahl sein!")
+        
+        breitenkopie = breite
+
+        for i in range(0,breite//2):
+            mitte = breite//2
+            print(i*" " + "X" + (mitte-1)*" " + "X" + (mitte-1)*" "+"X")
+            breite -=2
+
+        print(breitenkopie*"X")
+
+
+
+    except Exception as ex:
+        print(ex)
+
+#print(7//2)
+stern(5)
+
+
+'''breite = breitenkopie
+for i in range(breite // 2 -1, -1, -1):
+    #print(i)
+    mitte = breite // 2
+    #print(f"mitte{mitte}")
+    print(i * " " + "X" + (mitte - 3) * " " + "X" + (mitte - 3) * " " + "X")
+    breite += 2'''
