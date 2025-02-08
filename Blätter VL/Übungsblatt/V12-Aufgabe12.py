2d7a96e33ff630f0313b767025e276c4d0413a65
grisoustyle <nicolaspfeifer293@gmail.com>
2025-02-08 15:03:11 +0100

    v12a12-text

--- Blätter VL/Übungsblatt/V12-Aufgabe12.py
+++ Blätter VL/Übungsblatt/V12-Aufgabe12.py
@@ -5,3 +5,8 @@ def multiply(input_number_a,input_number_b):
         return input_number_a+ multiply(input_number_a,input_number_b-1)
 
 print(multiply(3,3))
+
+'''rekursive algorithmen rufen sich selbst auf um ein problem schrittweise zu lösen.
+die funktionen rufen sich so lange erneut auf bis eine abbruchbedingung erreicht ist.
+vorteil ist meist weniger schreibaufwand beim code - rekursiv ist relativ schlank je nach formel
+nachteil ist hoher speicherverbrauch durch ständig erneute aufrufung und finale ausrechnung erst am ende; code schwerer nachvollziehbar''' 
