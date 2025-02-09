98b56f167475c57a75594e5c39e672e98f2bfcd7
grisoustyle <nicolaspfeifer293@gmail.com>
2025-02-09 23:56:51 +0100

    k6a2-ganz

--- Blätter VL/Blatt 6/V6-Aufgabe2/K6-Aufgabe2.py
+++ Blätter VL/Blatt 6/V6-Aufgabe2/K6-Aufgabe2.py
@@ -1,6 +1,6 @@
 #f(x)= x^3 - 2x^2 + 3x - 5
 
-def nullstelle(zahl=0):
+def nullstelle_nr(zahl=0):
     def f(x):
         return x**3 - 2*x**2 + 3*x - 5
     def f2(x):
@@ -8,9 +8,33 @@ def nullstelle(zahl=0):
     
     epsilon = 0.001
     schätzung = 1
+    iteration = 1
 
     while abs(f(schätzung)-zahl)>=epsilon:
         schätzung = schätzung-f(schätzung)/f2(schätzung)
-    return schätzung
+        iteration +=1
+    return schätzung,iteration
 
-print(nullstelle())
+print(nullstelle_nr())
+
+def nullstelle_bin(zahl=0):
+    def f(x):
+        return x**3 - 2*x**2 + 3*x - 5
+    
+    untereGrenze = 0
+    obereGrenze = 2
+    epsilon = 0.001
+    iteration = 1
+
+    schätzung = untereGrenze + (obereGrenze-untereGrenze)/2
+
+    while abs(f(schätzung)-zahl)>=epsilon:
+        if f(schätzung) < zahl:
+            untereGrenze = schätzung
+        else:
+            obereGrenze = schätzung
+        schätzung = untereGrenze + (obereGrenze-untereGrenze)/2
+        iteration +=1
+    return schätzung,iteration
+
+print(nullstelle_bin())
